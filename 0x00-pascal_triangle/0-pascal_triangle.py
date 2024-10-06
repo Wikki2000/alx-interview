b96127bf8902f16763f11e6e859cb5e6e4cf9033
wikki2000 <wisdomokposin@gmail.com>
2024-10-06 17:13:02 +0100

    Move 0-pascal_triangle.py to 0x00-pascal_triangle directory

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Print the pascal numbers. """
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row always assign to 1
+
+    # Iterate from row index 1
+    for i in range(1, n):
+        row = [1]  # Pascal triangle starts with 1
+
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+        row.append(1)  # Last item ends with one
+
+        triangle.append(row)
+
+    return triangle
